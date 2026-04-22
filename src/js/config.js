507dd497b32f24db0189443074cc45ff7599a5fd
apalac13 <anabradaric77@gmail.com>
2026-04-22 10:49:17 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'add37e49-0880-4e9d-82bb-0ca0cc822f61';
+export const MODAL_CLOSE_SEC = 2.5;
